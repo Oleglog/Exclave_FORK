8355751c046e475298db351c93c4141bbc02ff24
juushimatsu <128093068+juushimatsu@users.noreply.github.com>
2026-05-04 18:05:18 +0400

    build.bat

--- library/core/build.bat
+++ library/core/build.bat
@@ -2,7 +2,7 @@
 
 set CGO_LDFLAGS=-Wl,-z,max-page-size=16384
 
-gomobile bind -v -androidapi 21 -trimpath -ldflags="-s -buildid=" -tags="with_clash" "github.com/dyhkwong/libsagernetcore"
+gomobile bind -v -androidapi 21 -trimpath -ldflags="-s -buildid= -checklinkname=0" -tags="with_clash" -o libsagernetcore.aar "github.com/dyhkwong/libsagernetcore" "github.com/openlibrecommunity/olcrtc/mobile"
 if errorlevel 1 (
     exit /b 1
 )
